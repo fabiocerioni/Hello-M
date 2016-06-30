0f135bf68a917965ee81aeb4fa881477ba08fe5a
fabiocerioni <fabio.cerioni@gmail.com>
2016-06-30 23:10:31 +0200

    teste
    fabio test

---
+++ bkind.js
@@ -0,0 +1,256 @@
+[{
+        "systid": ""
+        , "tadir_total": 4452872
+        , "n_obj_cli": 38558
+        , "p_obj_cli": "0.87"
+        , "tadir_tran": 151286
+        , "n_tra_cli": 1199
+        , "p_tra_cli": "0.86"
+        , "tadir_prog": 367626
+        , "n_pro_cli": 7285
+        , "p_pro_cli": "1.98"
+        , "n_pro_exe_cli": 4142
+        , "n_mf_cli": "Function Modules: 7209"
+        , "n_form_cli": 360
+        , "months": 1
+        , "cmo2": "     4123"
+        , "cmo4": "       19"
+        , "cmo5": "      134"
+        , "cmo6": "     1189"
+        , "cmo7": "       10"
+        , "cmo8": "      551"
+        , "mod_obj_tot": 700
+        , "mod_obj_per": 0
+        , "enhancements": 501
+        , "transports": 6
+        , "transports_err": 0
+        , "mont_av_ot": 6
+        , "n_obj_trans": 581
+        , "obj_av_ot": 97
+        , "obj_av_ot_mont": 581
+        , "code_lines": 2805164
+        , "oss_notes": 0
+        , "total_users": 5304
+        , "n_act_users": 2624
+        , "p_users": "49.47"
+        , "av_dia_resptime": "1451"
+        , "av_mont_resptime": "122461"
+        , "ndumps": 0
+        , "nsteps": 1291525
+        , "ndiasteps": 156251
+        , "act_users": 2680
+        , "num_sap_all": 567
+        , "num_exp": 0
+        , "n_user30": 3870
+        , "n_user90": 3852
+        , "account_exp": 98
+        , "account_l": 1
+        , "num_prof": 3497
+        , "un_prof": 2838
+        , "qrfc_in": 0
+        , "qrfc_out": 0
+        , "idoc_in": 0
+        , "idoc_out": 241
+        , "trfc_in": 0
+        , "trfc_out": 8
+        , "num_sost": 30
+        , "db_size": 0
+        , "db_used": 0
+        , "db_free": 0
+        , "growth_30": 0
+        , "growth_180": 0
+        , "miss_index": 0
+        , "avg_min": 0
+        , "dba_alert": 0
+        , "job_alert": 0
+        , "elems": {
+            "elemid": ""
+            , "elemtp": ""
+            , "created_on": "0000-00-00"
+            , "author": ""
+            , "mod": []
+        }
+    }, {
+        "systid": ""
+        , "tadir_total": 0
+        , "n_obj_cli": 0
+        , "p_obj_cli": "0.00"
+        , "tadir_tran": 0
+        , "n_tra_cli": 0
+        , "p_tra_cli": "0.00"
+        , "tadir_prog": 0
+        , "n_pro_cli": 0
+        , "p_pro_cli": "0.00"
+        , "n_pro_exe_cli": 0
+        , "n_mf_cli": ""
+        , "n_form_cli": 0
+        , "months": 0
+        , "cmo2": ""
+        , "cmo4": ""
+        , "cmo5": ""
+        , "cmo6": ""
+        , "cmo7": ""
+        , "cmo8": ""
+        , "mod_obj_tot": 0
+        , "mod_obj_per": 0
+        , "enhancements": 0
+        , "transports": 0
+        , "transports_err": 0
+        , "mont_av_ot": 0
+        , "n_obj_trans": 0
+        , "obj_av_ot": 0
+        , "obj_av_ot_mont": 0
+        , "code_lines": 0
+        , "oss_notes": 0
+        , "total_users": 0
+        , "n_act_users": 0
+        , "p_users": "0.00"
+        , "av_dia_resptime": "0"
+        , "av_mont_resptime": "0"
+        , "ndumps": 0
+        , "nsteps": 0
+        , "ndiasteps": 0
+        , "act_users": 0
+        , "num_sap_all": 0
+        , "num_exp": 0
+        , "n_user30": 0
+        , "n_user90": 0
+        , "account_exp": 0
+        , "account_l": 0
+        , "num_prof": 0
+        , "un_prof": 0
+        , "qrfc_in": 0
+        , "qrfc_out": 0
+        , "idoc_in": 0
+        , "idoc_out": 0
+        , "trfc_in": 0
+        , "trfc_out": 0
+        , "num_sost": 0
+        , "db_size": 0
+        , "db_used": 0
+        , "db_free": 0
+        , "growth_30": 0
+        , "growth_180": 0
+        , "miss_index": 0
+        , "avg_min": 0
+        , "dba_alert": 0
+        , "job_alert": 0
+        , "elems": {
+            "elemid": "R3TR"
+            , "elemtp": "ENHO"
+            , "created_on": "2015-10-26"
+            , "author": "CERIONI"
+            , "mod": [{
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "ERBSE"
+                        , "user_pack": "KLOSES"
+                        , "date": "1997-01-30"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": ""
+                        , "user_pack": ""
+                        , "date": "0000-00-00"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-73105INSAPBSFND"
+                        , "user_pack": "SAP"
+                        , "date": "2012-11-28"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPKCCG700"
+                        , "user_pack": "DDIC"
+                        , "date": "2005-12-03"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-73105INSAPBSFND"
+                        , "user_pack": "SAP"
+                        , "date": "2012-11-28"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPKCCG700"
+                        , "user_pack": "DDIC"
+                        , "date": "2005-12-03"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "ERBSE"
+                        , "user_pack": "KLOSES"
+                        , "date": "1997-01-30"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "/EDGE/DOCUMENT_CONNECTOR"
+                        , "task": "SAPK700HDC"
+                        , "user_pack": "SAPUSER"
+                        , "date": "2005-12-09"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "/EDGE/DOCUMENT_CONNECTOR"
+                        , "task": "SAPK700HDC"
+                        , "user_pack": "SAPUSER"
+                        , "date": "2005-12-09"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "/IG4S/DDICT"
+                        , "task": "SAPK-603DFINEAAPPL"
+                        , "user_pack": "EAAPPLUSER"
+                        , "date": "2008-02-06"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "/IG4S/DDICT"
+                        , "task": "SAPKCCG700"
+                        , "user_pack": "DDIC"
+                        , "date": "2005-12-03"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-702ADINSRMSRV"
+                        , "user_pack": "SAP"
+                        , "date": "2012-08-15"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-702ADINSRMSRV"
+                        , "user_pack": "SAP"
+                        , "date": "2012-08-15"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-702ADINSRMSRV"
+                        , "user_pack": "SAP"
+                        , "date": "2012-08-15"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-702ADINSRMSRV"
+                        , "user_pack": "SAP"
+                        , "date": "2012-08-15"
+                    }, {
+                        "modif": "0000-00-00"
+                        , "user_mod": ""
+                        , "package": "$TMP"
+                        , "task": "SAPK-702ADINSRMSRV"
+                        , "user_pack": "SAP"
+                        , "date": "2012-08-15"
+                    }, {
+                        "modif": "
